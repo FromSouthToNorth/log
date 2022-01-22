0a28c734cfc14dc906836d3bc723d8c4bf34a1ef
huangyu <598050554@qq.com>
2022-01-22 16:06:21 +0800

    修改一些对象

--- log-system-ui/src/router/index.js
+++ log-system-ui/src/router/index.js
@@ -181,7 +181,6 @@ export const constantRoutes = [
 
 export default new Router({
   mode: 'history', // 去掉url中的#
-  base: process.env.BASE_URL,
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
