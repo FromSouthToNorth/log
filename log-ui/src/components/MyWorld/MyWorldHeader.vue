9848caf386b4166170cfc95085d14b09b64d4962
huangyu <598050554@qq.com>
2022-02-25 17:14:54 +0800

    添加置顶文章封面动画

---
+++ log-ui/src/components/MyWorld/MyWorldHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="my-world-header"></div>
+</template>
+
+<script>
+import { init } from "@/components/MyWorld/myWorld";
+
+export default {
+  name: 'MyWorldHeader',
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    initWorld() {
+      init(".my-world-header")
+    }
+  }
+}
+</script>
+
+<style scoped>
+.my-world-header {
+  padding-bottom: 100px;
+  overflow: hidden;
+}
+</style>
---
+++ log-ui/src/views/aboutMe/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page__content">
+    <my-world-header />
+  </div>
+</template>
+
+<script>
+import MyWorldHeader from "@/components/MyWorld/MyWorldHeader";
+
+export default {
+  name: "index",
+  components: {MyWorldHeader}
+}
+</script>
+
+<style scoped>
+
+</style>
