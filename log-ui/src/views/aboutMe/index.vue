6a65fba10f7a11c3c42aa887c1101e601e272b16
huangyu <598050554@qq.com>
2022-03-02 15:34:44 +0800

    修复路由

--- log-ui/src/views/aboutMe/index.vue
+++ log-ui/src/views/aboutMe/index.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="page__content">
-    <my-world-header />
-  </div>
+  <section class="hy-section hy-about-section">
+    <div class="hy-container">
+      <div class="hy-col-4 hy-col-md-6 hy-col-sm-12 hy-offset-top-48 about">
+        <div class="about__img-wrapper">
+          <img src="https://202007002.oss-cn-chengdu.aliyuncs.com/avatar/2022/02/10/e14e068b-3dfe-48c5-9b60-95bbdeecb0cc.jpeg?versionId=CAEQTRiBgMCBga.M9xciIDBhMzBjZDM0ZDYwNDRhMDlhMzg5MDI1Mjk3ODk4M2Jj" alt="" class="about__img">
+        </div>
+        <p class="hy-text-2 hy-text-2_hardness_hard hy-text-2_theme_light hy-offset-top-24">
+        </p>
+        <p class="hy-text-2 hy-text-2_hardness_pale hy-text-2_theme_light hy-offset-top-24">
+          Hy
+          <br>
+          Java, Vue
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import MyWorldHeader from "@/components/MyWorld/MyWorldHeader";
 
 export default {
   name: "index",
-  components: {MyWorldHeader}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.about__img-wrapper {
+  overflow: hidden;
+  width: 72px;
+  height: 72px;
+  border-radius: 100%;
+}
+.about__img {
+  width: 100%;
+}
 
 </style>
